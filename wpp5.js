84b493511ff17ddef78763ded4873890bc218d43
meghraj-dewangan <meghrajdewangan.raipur@gmail.com>
2024-05-26 11:42:15 +0530

    remove character at the specified position

---
+++ wpp5.js
@@ -0,0 +1,13 @@
+// Remove a character at the specified position of a given string and return the new string
+
+
+
+
+function removeCharacter(string, position) {
+    
+    
+    return string.slice(0, position)+string.slice(position+1) 
+}
+ 
+console.log(removeCharacter("javaScript",4));
+console.log(removeCharacter("javaScript",2))
